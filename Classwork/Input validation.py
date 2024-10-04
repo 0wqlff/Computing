77a9a7b58059b7946174203a211141497f07c840
Ben Allan <B.Allan@albynschool.org>
2024-10-04 12:16:36 +0100

    git commit

---
+++ Classwork/Input validation.py
@@ -0,0 +1,10 @@
+menu=int(input("""Choose one of the following options:
+              1. View chores
+              2. Mark chore as completed
+              3. View completed chores
+              4. Exit
+               > """))
+while (menu<=1 and menu>=4):
+    print("success")
+else:
+    print("Error")
---
+++ Classwork/Running total.py
@@ -0,0 +1,15 @@
+#total = 0
+
+#for index in range(12):
+#    thismonth=int(input("How much did you save this month? £"))
+#    total += thismonth
+#    print("Your savings so far are",total,"pounds")
+
+#total = 0
+
+#for index in range(5):
+#    thistest=int(input("What percent did you get on this test"))
+#    total += thistest
+#    print("your overall score so far is",total)
+
+
