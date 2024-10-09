c9514a8fef3590772fc95d00f5959e13c9f639fe
Ben Allan <B.Allan@albynschool.org>
2024-10-09 09:05:16 +0100

    git commit

--- Classwork/Traversing a 1D Array.py
+++ Classwork/Traversing a 1D Array.py
@@ -1,7 +1,15 @@
-total=0
-temperature=[1,2,3,4,5,6,7]
+#total=0
+#temperature=[1,2,3,4,5,6,7]
 
-for index in range(7):
-    total += temperature[index]
+#for index in range(7):
+#    total += temperature[index]
 
-print("The average temperature of the week is",total/7)
+#print("The average temperature of the week is",total/7)
+
+#total=0
+#dogfoodW=[50.100,150,200,250]
+
+#for index in range(5):
+#    total += dogfoodW[index]
+
+#print("The toal dog food given to the dog was", total)
